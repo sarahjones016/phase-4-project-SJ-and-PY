4ce3efd83f48c6f69063005010eb180189d10dca
rknm-cell <racknahm@gmail.com>
2023-06-04 19:37:42 -0400

    added wine dictionary to seed file and created tables

--- server/app.py
+++ server/app.py
@@ -1,16 +1,74 @@
 #!/usr/bin/env python3
 
 # Standard library imports
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
 
-# Local imports
-from config import app, db, api
-from models import User, Recipe
 
+
+from models import db, User, Product, User_Product, User
+CORS(app)
+migrate = Migrate(app, db)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db.init_app(app)
+api = Api(app)
 # Views go here!
+
+class Users(Resource):
+    def get(self):
+        pass
+    
+api.add_resource(Users, '/users')
+
+
+class UsersById(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+api.add_resource(UsersById, '/users/<int:id>')
+
+
+class User_Products(Resource):
+    def get(self):
+        pass
+    
+api.add_resource(User_Products, '/userproducts')
+
+class Transactions(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+api.add_resource(Transactions, '/transactions')
+
+
+class TransactionsById(Resource):
+    def get(self):
+        pass
+    def post(self):
+        pass
+    def patch(self):
+        pass
+    def delete(self):
+        pass
+api.add_resource(TransactionsById, '/transactions/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,49 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
+db = SQLAlchemy(metadata=metadata)
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique = True, nullable = False)
+    password = db.Column(db.String)
+    admin = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return f'<User {self.id}: {self.name}>'
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    description = db.Column(db.String)
+    units = db.Column(db.Integer)
+    units_sold = db.Column(db.Integer)
+    image_url = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Product {self.id}: {self.name}>'
+class User_Product(db.Model, SerializerMixin):
+    __tablename__ = 'user_products'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+
+    def __repr__(self):
+        return f'<User Product {self.id}>'
+class Transaction(db.Model, SerializerMixin):
+    __tablesname__ = 'transactions'
+    id = db.Column(db.Integer, primary_key=True)
+    total_price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __repr__(self):
+        return f'<Transaction {self.id}>'
 
 # Models go here!
